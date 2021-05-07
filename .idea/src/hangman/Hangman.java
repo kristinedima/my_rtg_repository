f46c4cd9c4f792c2ff868f5445ba5e7c02aa1be0
kristine.dima <bruvere.kristine@gmail.com>
2021-05-07 20:42:25 +0300

    Hangman

--- .idea/src/hangman/Hangman.java
+++ .idea/src/hangman/Hangman.java
@@ -19,38 +19,14 @@ public class Hangman {
         if (option < 1 || option > 4) {
             System.out.println("You have enetered wrong option, please enter number between 1 - 4");
         } else if (option == 1) {
-            System.out.println("Enter your name");
-            String name = scanner.nextLine();
-            System.out.println("Enter your age");
+            System.out.println("Enter your name: ");
+            String name = scanner.next();
+            System.out.println("Welcome " + name + "," + "Enter your age: ");
             int age = scanner.nextInt();
 
         }
     }
 }
 
-
-//        System.out.println("Please choose an option");
-//        System.out.println("1 - login, 2 - log out, 3 - exit program");
-//        int option = scanner.nextInt();
-//        if (option < 1 || option > 3) {
-//            System.out.println("You have enetered wrong option, please enter number between 1 - 3");
-//        } else if (option == 1) {
-//                System.out.println("Enter your name");
-//                String name = scanner.nextLine();
-//                System.out.println("Enter your age");
-//                int age = scanner.nextInt();
-//            }
-
-//            switch (option) {
-//                case 1:
-//                    System.out.println("LOGIN");
-//                    break;
-//                case 2:
-//                    System.out.println("LOG OUT");
-//                    break;
-//                case 3:
-//                    System.out.println("EXIT PROGRAM");
-//                    break;
-//            }
 
 //        scanner.close();
