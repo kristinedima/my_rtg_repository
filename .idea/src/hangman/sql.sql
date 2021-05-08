e7a1203dd05100edf49701f29c0ac457e378cbf9
curious-odd-man <vlad.varslavans@gmail.com>
2021-05-08 10:49:43 +0300

    Update sql.sql

--- .idea/src/hangman/sql.sql
+++ .idea/src/hangman/sql.sql
@@ -1,16 +1,19 @@
 CREATE TABLE IF NOT EXISTS Dictionary (
-id INT NOT NULL,
+id INT NOT NULL,    -- For unique identifiers i suggest using automatically generated ID's. http://www.h2database.com/html/datatypes.html#identity_type
+  -- additionally you should make sure that ID are unique, otherwise it will not uniquly identify a row. So you should either use PRIMARY KEY, or UNIQUE. PK is preferred
 word VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Users (
-id INTEGER,
+id INTEGER,   -- Same here, i guess. Though I told about 3 fields for a user: username, name and age. And you can use username as a PRIMARY KEY and then you don't need additional ID column
 name VARCHAR(200) NOT NULL,
 age INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Game (
-games_played INT NULL,
+games_played INT NULL,  -- Will you accumulate here number of games played? Meaning 1 user will have 1 row in this table? This is not very flexible :)
+  -- I suggest to have one row per game. Then you don't have to store number of games played, you can always COUNT the rows for a user.
+  -- Additionally you don't have any reference for a user here, so how do you suppose to know who of all users played specific game?
 guesses INT NULL,
 outcome VARCHAR(200) NULL
 );
